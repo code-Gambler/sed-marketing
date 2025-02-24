44c2d340d6354fd9f90d622f8ab7185368a26e35
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2025-02-24 13:03:37 -0500

    Added nav elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -6,23 +6,52 @@ import { Link as ScrollLink, Element } from "react-scroll";
 
 export default function Home() {
   return (
-<div className="overflow-clip inset-0 -z-10 h-screen w-full bg-[#fafafa] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-  <Element 
-  name="top"
-  className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 
+    <div className="overflow-clip inset-0 -z-10 h-screen w-full bg-[#fafafa] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+      <Element
+        name="top"
+        className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 
   xl:w-4/5 2xl:w-[68%] bg-white flex items-center
   justify-between py-6 px-4 md:px-8 mx-6"
-  >
-    <Link href="/">
-    <Image
-    src={'/logo/logo.webp'}
-    alt="Sed Marketing"
-    width={1000}
-    height={1000}
-    className="w-28"
-    />
-    </Link>
-  </Element>
-</div>
+      >
+        <Link href="/">
+          <Image
+            src={"/logo/logo.webp"}
+            alt="Sed Marketing"
+            width={1000}
+            height={1000}
+            className="w-28"
+          />
+        </Link>
+
+        <div className="absolute right-1/2 translate-x-1/2 transform">
+          <div className="hidden md:flex gap-x-10 items-center text-gray-700 font-medium text-lg cursor-pointer">
+            <Link href={"/showcase"} className="hover:text-blue-500">
+              Showcase
+            </Link>
+            <ScrollLink
+              to="services"
+              smooth={true}
+              className="hover:text-blue-500"
+            >
+              Services
+            </ScrollLink>
+            <ScrollLink
+              to="process"
+              smooth={true}
+              className="hover:text-blue-500"
+            >
+              Process
+            </ScrollLink>
+            <ScrollLink
+              to="guarantees"
+              smooth={true}
+              className="hover:text-blue-500"
+            >
+              Guarantees
+            </ScrollLink>
+          </div>
+        </div>
+      </Element>
+    </div>
   );
 }
